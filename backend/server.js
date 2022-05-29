2c1d905ff75e4b8ee753806b2a7e7bffed574e00
dhruv50ae <linkdp2012@gmail.com>
2022-05-29 12:05:50 +0100

    Work on backend server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) =>p._id === req.params.id)
+    res.json(product);
+});
+
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
