b4eda1105445e7ec8e8dc121ef7a20b7d30aa775
dhruv50ae <linkdp2012@gmail.com>
2022-05-30 22:00:26 +0100

    implement spinner and message

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-import productListReducer from "./reducers/productReducers";
+import { productListReducer } from "./reducers/productReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
