91747c01a1102e89363f4224334daf15b3b389a1
dhruv50ae <linkdp2012@gmail.com>
2022-05-30 12:04:58 +0100

    Use asyncHandler to fix issues

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
