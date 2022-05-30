709d1a5cabc6dc182dd4705959c545b69f23ffd4
dhruv50ae <linkdp2012@gmail.com>
2022-05-30 21:33:46 +0100

    Add redux logic

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-const reducer = combineReducers({});
+import productListReducer from "./reducers/productReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
