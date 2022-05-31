9bea3476cd20bc4fa0c69c4be54aee51f04ba1b1
dhruv50ae <linkdp2012@gmail.com>
2022-05-31 19:43:31 +0100

    Encrypt user logins

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,14 @@ userSchema.methods.matchPasswords = async (enteredPassword) => {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userController.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(getUserProfile);
 
