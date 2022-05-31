f0de74919ce577a7b36b22ace2d7c94b98067382
dhruv50ae <linkdp2012@gmail.com>
2022-05-31 18:47:43 +0100

    Cleaned up backend route

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -24,7 +24,6 @@ const CartScreen = () => {
   const cart = useSelector((state) => state.cart);
 
   const { cartItems } = cart;
-  console.log(cartItems);
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
