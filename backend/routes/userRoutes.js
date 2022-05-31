27d26aea7cce1650ccfcb15db10d20e9ae3cbacf
dhruv50ae <linkdp2012@gmail.com>
2022-05-31 19:36:48 +0100

    errors and user auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
 
 router.post("/login", authUser);
+router.route("/profile").get(getUserProfile);
 
 export default router;
